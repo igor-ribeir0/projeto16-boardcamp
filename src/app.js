93f8c677e01d859b57e3a4f7d6b293b370f7eee2
igor-ribeir0 <igor_ribeirosantos@outlook.com>
2023-02-09 16:26:53 -0300

    feat: GET/games, POST/games

---
+++ src/app.js
@@ -0,0 +1,67 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import joi from "joi";
+import pg from "pg";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.listen(process.env.PORT);
+
+const connection = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+if(process.env.MODE === "prod"){
+    connection.ssl = true;
+};
+
+const newGameSchema = joi.object(
+    {
+        name: joi.string().required(),
+        image: joi.string().uri().required(),
+        stockTotal: joi.number().integer().min(1).required(),
+        pricePerDay: joi.number().integer().min(1).required()
+    }
+);
+
+app.get("/games", async(req, res) => {
+    try{
+        const gameList = await connection.query("SELECT * FROM games");
+        return res.status(200).send(gameList.rows);
+    }
+    catch(error){
+        return res.status(500).send(error.message);
+    }
+});
+
+app.post("/games", async(req, res) => {
+    const { name, image, stockTotal, pricePerDay } = req.body;
+    const newGame = { name, image, stockTotal, pricePerDay };
+    const validation = newGameSchema.validate(newGame, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(400).send(errors);
+    };
+
+    try{
+        const gameName = await connection.query(`SELECT * FROM games WHERE name = ${name}`);
+
+        if(gameName){
+            return res.sendStatus(409);
+        };
+
+        await connection.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ('$1', '$2', '$3', '$4')`, [name, image, stockTotal, pricePerDay]);
+
+        return res.sendStatus(201);
+    }
+    catch(error){
+        return res.status(500).send(error.message);
+    }
+});
