aa558b08049dfd546b1d1be6097c42cdafcb1f6d
igor-ribeir0 <igor_ribeirosantos@outlook.com>
2023-02-11 20:11:11 -0300

    feat: POST/rentals

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import joi from "joi";
+import dayjs from "dayjs";
 import pg from "pg";
 
 dotenv.config();
@@ -38,6 +39,14 @@ const customerSchema = joi.object(
         birthday: joi.date().max('2023-12-31').required()
     }
 );
+
+const rentalSchema = joi.object(
+    {
+        customerId: joi.number().positive().greater(0).required(),
+        gameId: joi.number().positive().greater(0).required(),
+        daysRented: joi.number().positive().greater(0).required()  
+    }
+)
 
 app.get("/games", async(req, res) => {
     try{
@@ -155,6 +164,63 @@ app.post("/customers", async(req, res) => {
 
         await connection.query(`INSERT INTO customers (name, phone, cpf, birthday) 
             VALUES ($1, $2, $3, $4)`, [name, phone, cpf, birthday]
+        );
+
+        res.sendStatus(201);
+    }
+    catch(error){
+        res.status(500).send(error.message);
+    }
+});
+
+app.post("/rentals", async(req, res) => {
+    const { customerId, gameId, daysRented } = req.body;
+    const newRental = { customerId, gameId, daysRented };
+    const validation = rentalSchema.validate(newRental, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(400).send(errors);
+    };
+
+    try{
+        const searchCustomer = await connection.query(`SELECT * FROM customers WHERE id = $1`, [customerId]);
+        const searchGame = await connection.query(`SELECT * FROM games WHERE id = $1`, [gameId]);
+        const rentalList = await connection.query(`SELECT * FROM rentals WHERE "gameId" = $1`, [gameId]);
+
+        if(searchCustomer.rows.length === 0 || searchGame.rows.length === 0){
+            return res.sendStatus(400);
+        };
+
+        if(rentalList.rows.length > searchGame.rows.stockTotal){
+            return res.sendStatus(400);
+        };
+
+        await connection.query(
+            `
+                INSERT INTO 
+                rentals 
+                (
+                    "customerId",
+                    "gameId",
+                    "rentDate",
+                    "daysRented", 
+                    "returnDate",
+                    "originalPrice",
+                    "delayFee"
+                )
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
+                
+            `,
+            [
+                customerId, 
+                gameId, 
+                dayjs().format("YYYY-MM-DD"), 
+                daysRented,
+                null,
+                daysRented * Number(searchGame.rows[0].pricePerDay) * 100,
+                null
+            ]
         );
 
         res.sendStatus(201);
