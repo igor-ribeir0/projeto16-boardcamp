5e187f169ed5976957c0632474e98b4d3cdc28c8
igor-ribeir0 <igor_ribeirosantos@outlook.com>
2023-02-12 11:58:46 -0300

    refector PUT/customers/id

--- src/controller/Customers.js
+++ src/controller/Customers.js
@@ -75,12 +75,12 @@ export async function customerUpdate(req, res){
             return res.sendStatus(404);
         };
 
-        if(customerCpf.rows.length >= 2) return res.sendStatus(409);
+        if(customerCpf.rows.length !== 0) return res.sendStatus(409);
 
-        await connection.query("UPDATE customers SET name = $1 WHERE id = $2", [name, id]);
-        await connection.query("UPDATE customers SET phone = $1 WHERE id = $2", [phone, id]);
-        await connection.query("UPDATE customers SET cpf = $1 WHERE id = $2", [cpf, id]);
-        await connection.query("UPDATE customers SET birthday = $1 WHERE id = $2", [birthday, id]);
+        await connection.query(
+			"UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5",
+			[name, phone, cpf, birthday, id]
+		);
 
         res.sendStatus(200);
     }
